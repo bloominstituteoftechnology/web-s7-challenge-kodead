c119c70fd7794e3bd4c158c383bc95eeb5663540
Ahmad Koroma <ahmadkoroma25@outlook.com>
2024-02-18 05:14:34 -0500

    done

--- mvpB.test.js
+++ mvpB.test.js
@@ -29,10 +29,31 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [5] renders a text that reads "JavaScript is pretty awesome"
     [6] renders a text that includes "javaScript is pretty" (use exact = false)
   */
-  test('you can comment out this test', () => {
-    expect(true).toBe(false)
-  })
+  // test('you can comment out this test', () => {
+  //   expect(true).toBe(false)
+  // })
 })
+describe('sum function unit tests', () => {
+  test('throws an error when no arguments are passed', () => {
+    expect(() => sum()).toThrow('pass valid numbers');
+  });
+
+  test('throws an error when one of the arguments is not a valid number', () => {
+    expect(() => sum(2, 'seven')).toThrow('pass valid numbers');
+  });
+
+  test('correctly sums two numbers', () => {
+    expect(sum(1, 3)).toBe(4);
+  });
+
+  test('correctly sums a string that can be converted to a number and a number', () => {
+    expect(sum('1', 2)).toBe(3);
+  });
+
+  test('correctly sums two strings that can be converted to numbers', () => {
+    expect(sum('10', '3')).toBe(13);
+  });
+});
 
 function sum(a, b) {
   a = Number(a)
@@ -42,6 +63,45 @@ function sum(a, b) {
   }
   return a + b
 }
+
+describe('HelloWorld component integration tests', () => {
+  test('renders a link that reads "Home"', () => {
+    render(<HelloWorld />);
+    const homeLink = screen.queryByText('Home');
+    expect(homeLink).toBeInTheDocument();
+  });
+
+  test('renders a link that reads "About"', () => {
+    render(<HelloWorld />);
+    const aboutLink = screen.queryByText('About');
+    expect(aboutLink).toBeInTheDocument();
+  });
+
+  test('renders a link that reads "Blog"', () => {
+    render(<HelloWorld />);
+    const blogLink = screen.queryByText('Blog');
+    expect(blogLink).toBeInTheDocument();
+  });
+
+  test('renders a text that reads "The Truth"', () => {
+    render(<HelloWorld />);
+    const theTruthText = screen.queryByText('The Truth');
+    expect(theTruthText).toBeInTheDocument();
+  });
+
+  test('renders a text that reads "JavaScript is pretty awesome"', () => {
+    render(<HelloWorld />);
+    const jsAwesomeText = screen.queryByText('JavaScript is pretty awesome');
+    expect(jsAwesomeText).toBeInTheDocument();
+  });
+
+  test('renders a text that includes "javaScript is pretty" (use exact = false)', () => {
+    render(<HelloWorld />);
+    const jsPrettyText = screen.queryByText('JavaScript is pretty', { exact: false });
+    expect(jsPrettyText).toBeInTheDocument();
+  });
+});
+
 
 function HelloWorld() {
   return (
